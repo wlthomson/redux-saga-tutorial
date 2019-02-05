4f5fa1973c3f58516206cd1bbae527babe5db50f
wlthomson <will.tho@outlook.com>
2019-02-06 02:23:44 +1300

    Add prettier config, hooks

--- main.js
+++ main.js
@@ -1,31 +1,32 @@
-import "babel-polyfill"
+import 'babel-polyfill';
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
-import createSagaMiddleware from 'redux-saga'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 
-import Counter from './Counter'
-import reducer from './reducers'
-import { helloSaga } from './sagas'
+import Counter from './Counter';
+import reducer from './reducers';
+import { helloSaga } from './sagas';
 
-const sagaMiddleware = createSagaMiddleware()
+const sagaMiddleware = createSagaMiddleware();
 
-const store = createStore(reducer, applyMiddleware(sagaMiddleware))
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
 
-sagaMiddleware.run(helloSaga)
+sagaMiddleware.run(helloSaga);
 
-const action = type => store.dispatch({type})
+const action = type => store.dispatch({ type });
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
       onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
+      onDecrement={() => action('DECREMENT')}
+    />,
     document.getElementById('root')
-  )
+  );
 }
 
-render()
-store.subscribe(render)
+render();
+store.subscribe(render);
